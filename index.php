17cfa2f1fd07d0acb5b14dca051f9ca3c6e9b0b7
keesiemeijer <keesie.meijer@gmail.com>
2017-01-17 19:54:52 +0100

    Update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $content = '';
 $output  = '';
 
 if ( isset( $_REQUEST['validate_code'] ) && isset( $_REQUEST['code_content'] ) ) {
-	if ( function_exists( '\WP_Parser_Validate\display_validation_errors' ) ) {
+	if ( function_exists( '\WP_Parser_Validate\get_validation_html' ) ) {
 
 		$content = (string) $_REQUEST['code_content'];
 		if ( get_magic_quotes_gpc() ) {
