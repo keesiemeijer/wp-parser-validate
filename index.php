3eff15352ee1cd2e78facac624cd1f3d41f9fcbe
keesiemeijer <keesie.meijer@gmail.com>
2017-01-24 12:56:05 +0100

    Rename variable $content to $validation

--- index.php
+++ index.php
@@ -3,20 +3,20 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
 }
 
-$content = '';
-$output  = '';
+$content     = '';
+$validation  = '';
 
 if ( isset( $_REQUEST['validate_code'] ) && isset( $_REQUEST['code_content'] ) ) {
 	if ( function_exists( '\WP_Parser_Validate\get_validation_html' ) ) {
 
 		$content = (string) $_REQUEST['code_content'];
 		if ( get_magic_quotes_gpc() ) {
-			$content = stripslashes( (string) $_REQUEST['code_content'] );
+			$content = stripslashes( $content );
 		}
 
-		$output = \WP_Parser_Validate\get_validation_html( $content );
+		$validation  = \WP_Parser_Validate\get_validation_html( $content );
+		$content     = htmlspecialchars( $content, ENT_QUOTES, 'UTF-8' );
 	}
 }
 
 include dirname( __FILE__ ) . "/lib/form.php";
-?>
