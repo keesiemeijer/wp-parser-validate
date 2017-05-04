91fec9ae8e33244517e8e37bb4d720f514e50af7
keesiemeijer <keesie.meijer@gmail.com>
2017-05-04 16:05:21 +0200

    move WP-CLI command out of file exists

--- parser-validate-command.php
+++ parser-validate-command.php
@@ -1,8 +1,10 @@
 <?php
+if ( ! class_exists( 'WP_CLI' ) ) {
+	return;
+}
+
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
-
-	if ( defined( 'WP_CLI' ) && WP_CLI ) {
-		\WP_CLI::add_command( 'parser', 'WP_Parser_Validate\\Command' );
-	}
 }
+
+\WP_CLI::add_command( 'parser', 'WP_Parser_Validate\\Command' );
