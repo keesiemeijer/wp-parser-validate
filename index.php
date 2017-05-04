30533ff68f11d5652b1d1dd86a59eb5f4e590cc0
keesiemeijer <keesie.meijer@gmail.com>
2017-05-04 13:18:42 +0200

    Define temp file path

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 $content     = '';
 $validation  = '';
 
+define( 'WP_PARSER_VALIDATE_TEMP_FILE', dirname( __FILE__ ) . '/content/temp.php' );
+
 if ( isset( $_REQUEST['validate_code'] ) && isset( $_REQUEST['code_content'] ) ) {
 	if ( function_exists( '\WP_Parser_Validate\get_validation_html' ) ) {
 
@@ -13,6 +15,7 @@ if ( isset( $_REQUEST['validate_code'] ) && isset( $_REQUEST['code_content'] ) )
 		if ( get_magic_quotes_gpc() ) {
 			$content = stripslashes( $content );
 		}
+
 
 		$validation  = \WP_Parser_Validate\get_validation_html( $content );
 		$content     = htmlspecialchars( $content, ENT_QUOTES, 'UTF-8' );
