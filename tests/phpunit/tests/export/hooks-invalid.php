642cb74a88902c703b7039a1d8638e6dfea2f766
keesiemeijer <keesie.meijer@gmail.com>
2017-05-05 13:20:04 +0200

    Add tests for hooks inside functions and methods

--- tests/phpunit/tests/export/hooks-invalid.php
+++ tests/phpunit/tests/export/hooks-invalid.php
@@ -48,8 +48,21 @@ class Validate_Hooks extends Export_UnitTestCase {
 	 * Test filter with concatenated name.
 	 */
 	public function test_filter_concatenated_name() {
-		//Invalid concatenated hook name for filter ''invalid_concatenated_name' . $filter'
-		$this->assertContainsSubstring( "Invalid concatenated hook name for filter ''invalid_concatenated_name' . " . '$filter' ."'", $this->logs );
+		$this->assertContainsSubstring( "Invalid concatenated hook name for filter ''invalid_concatenated_name' . " . '$filter' . "'", $this->logs );
+	}
+
+	/**
+	 * Test filter with concatenated name inside function.
+	 */
+	public function test_filter_concatenated_name_in_function() {
+		$this->assertContainsSubstring( "Invalid concatenated hook name for filter ''invalid_concatenated_name' . " . '$filter' . "' in function 'invalid_hooks_function'", $this->logs );
+	}
+
+	/**
+	 * Test filter with concatenated name inside function.
+	 */
+	public function test_filter_concatenated_name_in_method() {
+		$this->assertContainsSubstring( "Invalid concatenated hook name for filter ''invalid_concatenated_name' . " . '$filter' . "' in method 'invalid_hooks_method'", $this->logs );
 	}
 
 	/**
@@ -98,21 +111,21 @@ class Validate_Hooks extends Export_UnitTestCase {
 	 * Test filter with missing @param description.
 	 */
 	public function test_filter_missing_param_description() {
-		$this->assertContainsSubstring( "Missing description for @param '". '$value' . "' in DocBlock for filter 'missing_param_description_filter'", $this->logs );
+		$this->assertContainsSubstring( "Missing description for @param '" . '$value' . "' in DocBlock for filter 'missing_param_description_filter'", $this->logs );
 	}
 
 	/**
 	 * Test filter with mismatched @params.
 	 */
 	public function test_filter_mismatched_param() {
-		$this->assertContainsSubstring( "Mismatched @param tag name '". '$new_value' . "' in DocBlock for filter 'mismatched_params_filter'", $this->logs );
+		$this->assertContainsSubstring( "Mismatched @param tag name '" . '$new_value' . "' in DocBlock for filter 'mismatched_params_filter'", $this->logs );
 	}
 
 	/**
 	 * Test function with missing @param variable.
 	 */
 	public function test_filter_missing_param_variable() {
-		$this->assertContainsSubstring( "Missing @param variable '". '$value' . "' in DocBlock for filter 'missing_variable_filter'", $this->logs );
+		$this->assertContainsSubstring( "Missing @param variable '" . '$value' . "' in DocBlock for filter 'missing_variable_filter'", $this->logs );
 	}
 
 	/**
@@ -120,5 +133,19 @@ class Validate_Hooks extends Export_UnitTestCase {
 	 */
 	public function test_filter_not_succinct() {
 		$this->assertContainsSubstring( "Hook name could be more succinct for filter 'not_succinct_filter_name_{" . '$this' . "->filter}'", $this->logs );
+	}
+
+	/**
+	 * Test warning for not succinct hook name in function.
+	 */
+	public function test_filter_not_succinct_in_function() {
+		$this->assertContainsSubstring( "Hook name could be more succinct for filter 'not_succinct_filter_name_{" . '$this' . "->filter}' in function 'invalid_hooks_function'", $this->logs );
+	}
+
+	/**
+	 * Test warning for not succinct hook name in method.
+	 */
+	public function test_filter_not_succinct_in_method() {
+		$this->assertContainsSubstring( "Hook name could be more succinct for filter 'not_succinct_filter_name_{" . '$this' . "->filter}' in method 'invalid_hooks_method'", $this->logs );
 	}
 }
